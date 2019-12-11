81af4a65abcedb865358715249040ed5cfb69362
Jack Murray <jack@c0rporation.com>
2019-12-11 22:01:51 +0000

    Update calcs to include the fuel requirement for fuel

--- puzzle_1/puzzle_1.py
+++ puzzle_1/puzzle_1.py
@@ -6,6 +6,13 @@ inputs = []
 def calc_module_fuel(module_mass):
     return math.floor(module_mass / 3) - 2
 
+def calc_fuel_fuel(fuel_mass):
+    fuel = calc_module_fuel(fuel_mass)
+    if fuel < 0:
+        return 0
+    else:
+        return fuel + calc_fuel_fuel(fuel)
+
 def load_file(filename):
     with open(filename) as file_data:
         for line in file_data.readlines():
@@ -15,7 +22,9 @@ def calc_fuel_requirement():
     load_file("inputs/puzzle_1.txt")
     sum = 0
     for module in inputs:
-        sum += calc_module_fuel(module)
+        fuel = calc_module_fuel(module)
+        sum += fuel
+        sum += calc_fuel_fuel(fuel)
     return sum
 
 if __name__ == "__main__":
--- test/test_puzzle_1.py
+++ test/test_puzzle_1.py
@@ -9,4 +9,7 @@ class Puzzle1(unittest.TestCase):
         self.assertEqual(puzzle_1.puzzle_1.calc_module_fuel(100756), 33583)
     
     def test_puzzle_1_output(self):
-        self.assertEqual(puzzle_1.puzzle_1.calc_fuel_requirement(), 3184233)
+        self.assertEqual(puzzle_1.puzzle_1.calc_fuel_requirement(), 4773483)
+    
+    def test_puzzle_1_fuel(self):
+        self.assertEqual(puzzle_1.puzzle_1.calc_fuel_fuel(654), 312)
