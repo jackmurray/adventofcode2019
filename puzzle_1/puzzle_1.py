1348881eba8715f04766ad91755d9738355f2743
Jack Murray <jack@c0rporation.com>
2019-12-11 21:16:40 +0000

    Add inputs for puzzle 1 and add code to read the file and calculate the answer

--- puzzle_1/puzzle_1.py
+++ puzzle_1/puzzle_1.py
@@ -1,4 +1,19 @@
 import math
+import sys
+
+inputs = []
 
 def calc_module_fuel(module_mass):
     return math.floor(module_mass / 3) - 2
+
+def load_file(filename):
+    with open(filename) as file_data:
+        for line in file_data.readlines():
+            inputs.append(int(line))
+
+if __name__ == "__main__":
+    load_file("inputs/puzzle_1.txt")
+    sum = 0
+    for module in inputs:
+        sum += calc_module_fuel(module)
+    print(sum)
