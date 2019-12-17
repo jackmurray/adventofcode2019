d961bd09870233acdb2fd1069e85d6022e103b0f
Jack Murray <jack@c0rporation.com>
2019-12-17 17:38:28 +0000

    Change to using bitflags to indicate which wires have passed a particular point

--- puzzle_3.py
+++ puzzle_3.py
@@ -9,6 +9,7 @@ class WireGrid:
 
     def __init__(self):
         self.visit_list = defaultdict(lambda: defaultdict(int))
+        self.wire_flag = 1 # Bit that indicates which wire we're adding
 
     def add_wire(self, wire: Wire):
         for move in wire.path:
@@ -17,27 +18,28 @@ class WireGrid:
 
             if direction == 'U':
                 for i in range(1, distance + 1):
-                    self.visit_list[wire.head['x']][wire.head['y'] + i] += 1
+                    self.visit_list[wire.head['x']][wire.head['y'] + i] |= self.wire_flag
                 wire.head['y'] += distance
             if direction == 'D':
                 for i in range(1, distance + 1):
-                    self.visit_list[wire.head['x']][wire.head['y'] - i] += 1
+                    self.visit_list[wire.head['x']][wire.head['y'] - i] |= self.wire_flag
                 wire.head['y'] -= distance
             if direction == 'L':
                 for i in range(1, distance + 1):
-                    self.visit_list[wire.head['x'] - i][wire.head['y']] += 1
+                    self.visit_list[wire.head['x'] - i][wire.head['y']] |= self.wire_flag
                 wire.head['x'] -= distance
             if direction == 'R':
                 for i in range(1, distance + 1):
-                    self.visit_list[wire.head['x'] + i][wire.head['y']] += 1
-                wire.head['x'] += distance            
+                    self.visit_list[wire.head['x'] + i][wire.head['y']] |= self.wire_flag
+                wire.head['x'] += distance
+        self.wire_flag <<= 1
             
     
     def get_intersections(self):
-        # An intersection is any position on the grid with a value of 2.
+        # An intersection is any position on the grid with flags for wires 1 and 2 set.
         for x in self.visit_list.keys():
             for y in self.visit_list[x].keys():
-                if self.visit_list[x][y] == 2:
+                if self.visit_list[x][y] == 3: # Wires 1 and 2
                     yield x, y
 
     def get_shortest_intersection(self):
